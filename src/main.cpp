73f484e7e29e391415936bd672cad404c3203ab1
juthomas <juthomas@student.42.fr>
2021-01-20 16:39:55 +0100

    [ADD] sems to work !

--- src/main.cpp
+++ src/main.cpp
@@ -98,12 +98,12 @@ void setup() {
 
 	tft.drawString("Connecting", tft.width() / 2, tft.height() / 2);
 
-  magnetic_captor.initialize();
-  delay(1000);
-  while (!magnetic_captor.testConnection()) {
-    Serial.println("erreur connexion capteur HMC5883L..");
-    delay(500);
-  }
+	magnetic_captor.initialize();
+	delay(1000);
+	while (!magnetic_captor.testConnection()) {
+		Serial.println("erreur connexion capteur HMC5883L..");
+		delay(500);
+	}
 
 	Serial.print("hello\n");
 	Serial.print(".\n");
@@ -111,45 +111,191 @@ void setup() {
 }
 
 
+void drawBatteryLevel(TFT_eSprite *sprite, int x, int y, float voltage)
+{
+	uint32_t color1 = TFT_GREEN;
+	uint32_t color2 = TFT_WHITE;
+	uint32_t color3 = TFT_BLUE;
+	uint32_t color4 = TFT_RED;
+	if (voltage > 4.33)
+	{
+		(*sprite).fillRect(x, y, 30, 10, color3);
+	}
+	else if (voltage > 3.2)
+	{
+		(*sprite).fillRect(x, y, map((long)(voltage * 100), 320, 430, 0, 30), 10, color1);
+		(*sprite).setCursor(x + 7, y + 1);
+		(*sprite).setTextColor(TFT_DARKGREY);
+		(*sprite).printf("%02ld%%", map((long)(voltage * 100), 320, 432, 0, 100));
+	}
+	else
+	{
+		(*sprite).fillRect(x, y, 30, 10, color4);
+	}
+	(*sprite).drawRect(x, y, 30, 10, color2);
+}
+
+void print_activity(int angle, double mySpeed)
+{
+
+	TFT_eSprite drawingSprite = TFT_eSprite(&tft);
+	drawingSprite.setColorDepth(8);
+	drawingSprite.createSprite(tft.width(), tft.height());
+	
+	drawingSprite.fillSprite(TFT_BLACK);
+			drawingSprite.setTextSize(1);
+		drawingSprite.setTextFont(1);
+		drawingSprite.setTextColor(TFT_GREEN);
+		drawingSprite.setTextDatum(MC_DATUM);
+	drawingSprite.setCursor(0, 0);
+
+	uint16_t v = analogRead(ADC_PIN);
+	float battery_voltage = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
+		drawingSprite.setTextColor(TFT_RED);
+	drawingSprite.printf("Voltage : ");
+		drawingSprite.setTextColor(TFT_WHITE);
+	drawingSprite.printf("%.2fv\n\n", battery_voltage);
+	drawBatteryLevel(&drawingSprite, 100, 00, battery_voltage);
+	drawingSprite.setCursor(0, 20);
+
+	drawingSprite.setTextColor(TFT_RED);
+	drawingSprite.printf("Satellites : ");
+	drawingSprite.setTextColor(TFT_WHITE);
+	drawingSprite.printf("%ld\n\n", nbre_sat);
+	if (nbre_sat < 3)
+	{
+		drawingSprite.setTextSize(2);
+		drawingSprite.setTextFont(1);
+		drawingSprite.setTextColor(TFT_GREEN);
+		drawingSprite.setTextDatum(MC_DATUM);
+		drawingSprite.setCursor(0, 100);
+		drawingSprite.printf("Creation\ndu contact\navec les\nIlluminatis");
+		drawingSprite.setTextColor(TFT_DARKGREY);
+		drawingSprite.setTextSize(1);
+		drawingSprite.setCursor(0, 230);
+		drawingSprite.printf("juthomas.fr");
+
+		drawingSprite.pushSprite(0, 0);
+		drawingSprite.deleteSprite();
+	}
+
+	TFT_eSprite direction = TFT_eSprite(&tft);
+	direction.setColorDepth(8);
+	direction.createSprite(100, 20);
+	direction.fillRect(5, 5, 90, 10, TFT_BLUE);
+	direction.fillTriangle(90,0,100,10,90,20,TFT_BLUE);
+	direction.setPivot(50, 10);
+	// direction.setRotation(60);
+	
+	
+	// direction.pushSprite(50, 50);
+	TFT_eSprite directionBack = TFT_eSprite(&tft);
+	directionBack.setColorDepth(8);
+	directionBack.createSprite(100, 100);
+
+	direction.pushRotated(&directionBack, angle+90);
+	directionBack.pushToSprite(&drawingSprite, 15, 50);
+
+	drawingSprite.setTextSize(2);
+	drawingSprite.setTextFont(1);
+	drawingSprite.setTextColor(TFT_GREEN);
+	drawingSprite.setTextDatum(MC_DATUM);
+	drawingSprite.setCursor(0, 200);
+	drawingSprite.printf("%0.2lfkm/h", mySpeed);
+	drawingSprite.setTextColor(TFT_DARKGREY);
+	drawingSprite.setTextSize(1);
+	drawingSprite.setCursor(0, 230);
+	drawingSprite.printf("juthomas.fr");
+
+
+	drawingSprite.pushSprite(0, 0);
+	drawingSprite.deleteSprite();
+
+	// drawingSprite.setPivot(0, 0);
+	// drawingSprite.pushRotated(60);
+
+}
+
 
 void loop() {
+	static double mySpeed = 0;
+	static bool isCalibrated = false;
+	static int calMinX = 0;
+	static int calMaxX = 0;
+	static int calMinY = 0;
+	static int calMaxY = 0;
+
+
+
 	// Serial.print(".\n");
 	delay(100);
 	while (Serial3.available()) {
-    data = Serial3.read();
-    // Serial.print(data);
-    gps.encode(data);
-    if (gps.location.isUpdated() || gps.satellites.isUpdated())
-    {
-      latitude = gps.location.lat();
-      longitude = gps.location.lng();
-      alt = gps.altitude.meters();
-      vitesse = gps.speed.kmph();
-      nbre_sat = gps.satellites.value();
+		data = Serial3.read();
+		// Serial.print(data);
+		gps.encode(data);
+		if (gps.location.isUpdated() || gps.satellites.isUpdated())
+		{
+			latitude = gps.location.lat();
+			longitude = gps.location.lng();
+			alt = gps.altitude.meters();
+			vitesse = gps.speed.kmph();
+			nbre_sat = gps.satellites.value();
  
-      Serial.println("-------- FIX GPS ------------");
-      Serial.print("LATITUDE="); Serial.println(latitude);
-      Serial.print("LONGITUDE="); Serial.println(longitude);
-      Serial.print("ALTITUDE (m) ="); Serial.println(alt);
-      Serial.print("VITESSE (km/h)="); Serial.println(vitesse);
-      Serial.print("NOMBRE SATTELLITES="); Serial.println(nbre_sat);
-    }
-  }
+			Serial.println("-------- FIX GPS ------------");
+			Serial.print("LATITUDE="); Serial.println(latitude);
+			Serial.print("LONGITUDE="); Serial.println(longitude);
+			Serial.print("ALTITUDE (m) ="); Serial.println(alt);
+			Serial.print("VITESSE (km/h)="); Serial.println(vitesse);
+			Serial.print("NOMBRE SATELLITES="); Serial.println(nbre_sat);
+			Serial.print("VITESSE DE LA TERRE="); Serial.println(2 * PI * cos(radians(latitude)) * (6378 + alt) / 24);
+			mySpeed = 2 * PI * cos(radians(latitude)) * 6378 / 24 + vitesse;
+		}
+	}
+
+
 	magnetic_captor.getHeading(&mx, &my, &mz);
+	
+	if (isCalibrated == false)
+	{
+		calMinX = mx;
+		calMaxX = mx;
+		calMinY = my;
+		calMaxY = my;
+		isCalibrated = true;
+	}
+	else
+	{
+		if (mx < calMinX)
+			calMinX = mx;
+		else if (mx > calMaxX)
+			calMaxX = mx;
+		if (my < calMinY)
+			calMinY = my;
+		else if (my > calMaxY)
+			calMaxY = my;
+	}
+
+	// affichage des données
+	// Serial.println("-------------");
+	// Serial.print("mag:\t");
+	// Serial.print(mx);
+	// Serial.print("\t");
+	// Serial.print(my);
+	// Serial.print("\t");
+	// Serial.print(mz);
+	// Serial.println("\t");
  
-  // affichage des données
-  Serial.println("-------------");
-  Serial.print("mag:\t");
-  Serial.print(mx);
-  Serial.print("\t");
-  Serial.print(my);
-  Serial.print("\t");
-  Serial.print(mz);
-  Serial.println("\t");
- 
-  // calcul et affichage de l'angle en degrés par rapport au nord
-  angle = atan2((double)mx, (double)my);
-  Serial.print("angle:\t");
-  Serial.println(angle * 180 / PI);
-  delay(1000);
+	// calcul et affichage de l'angle en degrés par rapport au nord
+
+	int xcal = map(mx, calMinX, calMaxX, -1000, 1000);
+	int ycal = map(my, calMinY, calMaxY, -1000, 1000);
+
+	// int xcal = map(mx, -950, -150, -1000, 1000);
+	// int ycal = map(my, -260, 380, -1000, 1000);
+	
+	angle = atan2((double)xcal, (double)ycal);
+	// Serial.print("angle:\t");
+	// Serial.println(angle * 180 / PI);
+	print_activity(-(int)(angle * 180 / PI), mySpeed);
+	// delay(1000);
 }
